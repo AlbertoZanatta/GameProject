6e7c56f20835e0e157b0604044d1f65a12312f91
AlbertoZanatta <alberto.zanatta.3@studenti.unipd.it>
2018-10-09 14:45:15 +0200

    Commit iniziale
    Primo commit

---
+++ Prototype0/Assets/Scripts/Controller.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public float speed = 10f;
+    public float maxSpeed = 15f;
+    public float jumpForce = 3.2f;
+    public float jump = 3.5f;
+    public float jumpRatio = 0.4f;
+    public static bool left = false;
+
+    private Rigidbody2D body;
+    private bool hasFlag = false;
+    private bool isGrounded = true;
+    private SpriteRenderer renderer; 
+    
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float horiz = 0f;
+        float vert = 0f;
+
+        var absvx = Mathf.Abs(body.velocity.x);
+        if(isGrounded)
+        {
+            if(absvx < maxSpeed)
+            {
+                horiz = Input.GetAxis("Horizontal");
+                if(horiz != 0)
+                {
+                    bool moveLeft = horiz > 0 ? false : true;
+                    renderer.flipX = moveLeft;
+                    if(left != moveLeft)
+                    {
+                        left = moveLeft;
+                        body.velocity = Vector2.zero;
+
+                    }
+                    
+                }
+                
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isGrounded = false;
+                Vector2 jumpVec = left ? new Vector2(-jump*jumpRatio, jump) : new Vector2(jump*jumpRatio, jump);
+                body.AddForce(jumpVec * jumpForce, ForceMode2D.Impulse);
+            }else
+            {
+                Vector2 movement = new Vector2(horiz * speed, vert);
+                body.AddForce(movement);
+            }
+        }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("OnTriggerEnter");
+        GameObject colliding = collision.gameObject;
+        if (colliding.CompareTag("Flag"))
+        {
+            Debug.Log("Bandiera");
+            hasFlag = true;
+            colliding.SetActive(false);
+        }else if (colliding.CompareTag("Objective"))
+        {
+            if (hasFlag)
+            {
+                Debug.Log("You Have Won!");
+            }
+        }
+        else if (colliding.CompareTag("Enemy"))
+        {
+            Debug.Log("You lost");
+            //Restart();
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground")&&(!isGrounded))
+        {
+            isGrounded = true;
+        }
+    }
+
+    public bool HasFlag()
+    {
+        return hasFlag;
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+}
---
+++ Prototype0/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public GameObject player;
+    public float attackDist = 5f;
+    public float speed = 10;
+    public CircleCollider2D headCollider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        headCollider = GetComponent<CircleCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        if (distance <= attackDist)
+        {
+            Vector2 heading = player.transform.position - transform.position;
+            Vector2 direction = heading / distance;
+            float deltaSpeed = speed * Time.deltaTime;
+            transform.Translate(direction.x*deltaSpeed, 0.0f, 0.0f);
+        }
+
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player")&&collision.otherCollider == headCollider)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
