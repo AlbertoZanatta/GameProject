6dd2b254a17287a0aea76d6025725254a0c065cd
Tolis <lalosapost@gmail.com>
2018-12-11 13:31:58 +0100

    initial tutorial

---
+++ Prototype0/Assets/TutorialCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCollider : MonoBehaviour {
+    public GameObject screen;
+    public bool mode;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            screen.SetActive(mode);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    
+}
