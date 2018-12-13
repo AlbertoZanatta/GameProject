924d5730c74be43288c87dc5fc0cfa8cfc9d073f
AlbertoZanatta <alberto.zanatta.3@studenti.unipd.it>
2018-12-13 14:57:28 +0100

    Torch sound
    torch sound

---
+++ Prototype0/Assets/TorchSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchSound : MonoBehaviour {
+    private static int numTorches;
+    private int numTorch;
+
+    private void Start()
+    {
+        numTorches++;
+        numTorch = numTorches;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float playerDist = Mathf.Abs(transform.position.x -  PlayerController.Instance.transform.position.x);
+        SoundManager.instance.Torch(playerDist, numTorch);
+	}
+}
